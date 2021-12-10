d7925527bd0c2c9495dd8d692c8129a5bc69cd52
Vaibhav  Varshney <89084390+vaibhav0726@users.noreply.github.com>
2021-12-10 21:40:04 +0530

    To print process id and its name

---
+++ Process/process.sh
@@ -0,0 +1,7 @@
+# Write a shell script to display the process name and its process id.
+
+#to print current file name
+echo "Process Name Of Current File : $0"
+
+#to print current process id
+echo "Process Id Of Current File : $$"
