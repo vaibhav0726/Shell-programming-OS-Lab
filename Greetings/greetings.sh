9fa76c2c5d189a9df68875f82d5e358235c3636d
Vaibhav  Varshney <89084390+vaibhav0726@users.noreply.github.com>
2021-12-11 18:48:08 +0530

    To give greetings according to time

---
+++ Greetings/greetings.sh
@@ -0,0 +1,27 @@
+# . Write a shell script to take name as a input and display a greeting message to the
+# user by checking system clock. (Ex :- Hello John Good Morning in morning time else
+# Hello John Good Afternoon in afternoon time else Hello John Good Evening in
+# Evening time)
+
+echo "enter your name"
+read a
+
+#to get number of hours
+h=$(date +"%H")
+
+#comparing the hour 
+if [ $h -gt 6 -a $h -le 12 ]
+then
+echo "Hello $a Good Morning"
+
+elif [ $h -gt 12 -a $h -le 16 ]
+then
+echo "Hello $a Good Afternoon"
+
+elif [ $h -gt 16 -a $h -le 20 ]
+then
+echo "Hello $a Good Evening"
+
+else
+echo "Hello $a Good Night"
+fi
